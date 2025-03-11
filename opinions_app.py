720bf2b5a6570461325f12e09ca1106dbbcb1cfc
wabisabicode <tretyakov@uni-mainz.de>
2025-03-11 12:37:05 +0100

    import SQLAlchemy

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index_view():
     return 'Совсем скоро тут будет случайное мнение о фильме!'
+
 
 if __name__ == '__main__':
     app.run()
