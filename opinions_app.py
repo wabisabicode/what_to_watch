4ac1c003e421f9406dbfa8c9a4c46a3fd2fe78dc
wabisabicode <tretyakov@uni-mainz.de>
2025-03-11 14:10:04 +0100

    add class Opinion

--- opinions_app.py
+++ opinions_app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,6 +9,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 # sqlite:////username/dev/what_to_watch/db.sqlite3
 
 db = SQLAlchemy(app)
+
+class Opinion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    source = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
 
 
 @app.route('/')
