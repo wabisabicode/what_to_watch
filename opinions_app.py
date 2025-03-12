fea88aa46c598b541880f81247483a6dd059d97d
wabisabicode <tretyakov@uni-mainz.de>
2025-03-12 15:11:09 +0100

    rename args of render_template

--- opinions_app.py
+++ opinions_app.py
@@ -30,12 +30,12 @@ def index_view():
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
 
-    return render_template('index.html', opinion=opinion)
+    return render_template('opinion.html', opinion=opinion)
 
 
 @app.route('/add')
 def add_opinion_view():
-    return 'Страница в разработке!'
+    return render_template('add_opinion.html')
 
 
 if __name__ == '__main__':
