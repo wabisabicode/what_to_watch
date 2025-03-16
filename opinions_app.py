00821883a0f84950c7b824065d940f85cbd9d74d
wabisabicode <tretyakov@uni-mainz.de>
2025-03-16 09:54:59 +0100

    add flash messages

--- opinions_app.py
+++ opinions_app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from random import randrange
 
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, flash, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, URLField
@@ -57,9 +57,13 @@ def index_view():
 def add_opinion_view():
     form = OpinionForm()
     if form.validate_on_submit():
+        text = form.text.data
+        if Opinion.query.filter_by(text=text).first() is not None:
+            flash('Такое мнение уже было оставлено ранее!')
+            return render_template('add_opinion.html', form=form)
         opinion = Opinion(
             title=form.title.data,
-            text=form.text.data,
+            text=text,
             source=form.source.data
         )
         db.session.add(opinion)
