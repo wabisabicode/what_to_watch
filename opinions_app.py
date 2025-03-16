4abfda2377f8caaa19bdab8d8ba4d40277eaa4e0
wabisabicode <tretyakov@uni-mainz.de>
2025-03-16 14:23:53 +0100

    add cli command for uploading opinions from csv into database

--- opinions_app.py
+++ opinions_app.py
@@ -1,6 +1,8 @@
+import csv
 from datetime import datetime
 from random import randrange
 
+import click
 from flask import Flask, abort, flash, redirect, render_template, url_for
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -90,6 +92,20 @@ def internal_error(error):
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
+
+@app.cli.command('load_opinions')
+def load_opinions_command():
+    """Upload opinions into the database."""
+    with open('opinions.csv', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        counter = 0
+        for row in reader:
+            opinion = Opinion(**row)
+            db.session.add(opinion)
+            db.session.commit()
+            counter += 1
+    click.echo(f'Загружено мнений: {counter}')
 
 
 if __name__ == '__main__':
