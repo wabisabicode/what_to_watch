8ca1d11ae5e4f2d1eda5e3a98213f2ff5d29ee30
wabisabicode <tretyakov@uni-mainz.de>
2025-03-17 16:18:59 +0100

    add get_opinion in api

---
+++ opinions_app/api_views.py
@@ -0,0 +1,10 @@
+from flask import jsonify
+
+from . import app
+from .models import Opinion
+
+
+@app.route('api/opinions/<int:id>', methods=['GET'])
+def get_opinion(id):
+    opinion = Opinion.query.get_or_404(id)
+    return jsonify({'opinion': opinion}), 200
