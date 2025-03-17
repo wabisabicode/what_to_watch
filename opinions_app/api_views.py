98123416d1bd19973660f9872972e497684cc23e
wabisabicode <tretyakov@uni-mainz.de>
2025-03-17 16:25:37 +0100

    add opinion_to_dict

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -8,3 +8,14 @@ from .models import Opinion
 def get_opinion(id):
     opinion = Opinion.query.get_or_404(id)
     return jsonify({'opinion': opinion}), 200
+
+
+def opinion_to_dict(opinion):
+    return dict(
+        id=opinion.id,
+        title=opinion.title,
+        text=opinion.text,
+        source=opinion.source,
+        timestamp=opinion.timestamp,
+        added_by=opinion.added_by
+    )
