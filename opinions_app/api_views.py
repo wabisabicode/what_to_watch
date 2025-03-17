2735d3ee944e63927ca00783a6d42ae57fe23d80
wabisabicode <tretyakov@uni-mainz.de>
2025-03-17 22:42:22 +0100

    add delete_opinion to api_views.py

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -20,3 +20,11 @@ def update_opinion(id):
     opinion.added_by = data.get('added_by', opinion.added_by)
     db.session.commit()
     return jsonify({'opinion': opinion.to_dict()}), 200
+
+
+@app.route('/api/opinions/<int:id>/', methods=['DELETE'])
+def delete_opinion(id):
+    opinion = Opinion.query.get_or_404(id)
+    db.session.delete(opinion)
+    db.session.commit()
+    return '', 204
