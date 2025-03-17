495d55d65b2f0ff30ef5319988157af1b0b6f772
wabisabicode <tretyakov@uni-mainz.de>
2025-03-17 22:39:43 +0100

    add update_opinion to api_views

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -1,10 +1,22 @@
-from flask import jsonify
+from flask import jsonify, request
 
-from . import app
+from . import app, db
 from .models import Opinion
 
 
 @app.route('/api/opinions/<int:id>/', methods=['GET'])
 def get_opinion(id):
     opinion = Opinion.query.get_or_404(id)
+    return jsonify({'opinion': opinion.to_dict()}), 200
+
+
+@app.route('/api/opinions/<int:id>/', methods=['PATCH'])
+def update_opinion(id):
+    data = request.get_json()
+    opinion = Opinion.query.get_or_404(id)
+    opinion.title = data.get('title', opinion.title)
+    opinion.text = data.get('text', opinion.text)
+    opinion.source = data.get('source', opinion.source)
+    opinion.added_by = data.get('added_by', opinion.added_by)
+    db.session.commit()
     return jsonify({'opinion': opinion.to_dict()}), 200
